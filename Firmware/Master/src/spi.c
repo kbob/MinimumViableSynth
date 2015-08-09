edb3ea623290278a4df8e1f814df5d23eaefcd3f
kbob <kbob@jogger-egg.com>
2015-08-09 08:29:03 -0700

    Master now receives SPI data using DMA.

--- Firmware/Master/src/spi.c
+++ Firmware/Master/src/spi.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <libopencm3/stm32/dma.h>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/spi.h>
@@ -45,45 +46,79 @@
 //    Group C:  ?
 //    Group D:  ?
 
+// Each active DMA requires a controller/stream/channel triple.
+// Each controller/stream can only have a single channel active at a time.
+//
+// SPI 1 RX: 2/0/3, 2/2/3
+// SPI 1 TX: 2/3/3, 2/5/3
+//
+// SPI 3 RX: 1/0/0, 1/2/0
+// SPI 3 TX: 1/5/0, 1/7/0
+//
+// SPI 4 RX: 2/0/4, 2/3/5
+// SPI 4 TX: 2/1/4, 2/4/5
+//
+// SPI 5 RX: 2/3/2, 2/5/7
+// SPI 5 TX: 2/4/2, 2/6/7
+
+#define RX_DMA 1
+#define TX_DMA 0
+
 typedef struct gpio_pin {
     uint32_t gp_port;
     uint16_t gp_pin;
     uint8_t  gp_af;
 } gpio_pin;
 
+typedef struct DMA_channel {
+    uint32_t dc_dma;
+    uint8_t  dc_stream;
+    uint8_t  dc_channel;
+} DMA_channel;
+
 typedef struct spi_config {
     uint32_t sc_reg_base;
     gpio_pin sc_sck;
     gpio_pin sc_miso;
     gpio_pin sc_mosi;
+    DMA_channel sc_rx_dma;
+    DMA_channel sc_tx_dma;
 } spi_config;
 
 static const spi_config spi1_config = {
     .sc_reg_base = SPI1_BASE,
-    .sc_sck  = { GPIOA,  GPIO5, GPIO_AF5 },
-    .sc_miso = { GPIOB,  GPIO4, GPIO_AF5 },
-    .sc_mosi = { GPIOA,  GPIO7, GPIO_AF5 },
+    .sc_sck      = { GPIOA, GPIO5, GPIO_AF5 },
+    .sc_miso     = { GPIOB, GPIO4, GPIO_AF5 },
+    .sc_mosi     = { GPIOA, GPIO7, GPIO_AF5 },
+    .sc_rx_dma   = { DMA2,  DMA_STREAM2, 3 },
+    .sc_tx_dma   = { DMA2,  DMA_STREAM3, 3 },
 };
 
 static const spi_config spi3_config = {
     .sc_reg_base = SPI3_BASE,
-    .sc_sck  = { GPIOB,  GPIO3, GPIO_AF6 },
-    .sc_miso = { GPIOC, GPIO11, GPIO_AF6 },
-    .sc_mosi = { GPIOC, GPIO12, GPIO_AF6 },
+    .sc_sck      = { GPIOB, GPIO3, GPIO_AF6 },
+    .sc_miso     = { GPIOC, GPIO11, GPIO_AF6 },
+    .sc_mosi     = { GPIOC, GPIO12, GPIO_AF6 },
+    .sc_rx_dma   = { DMA1,  DMA_STREAM0, 0 },
+    .sc_tx_dma   = { DMA1,  DMA_STREAM5, 0 },
 };
 
 static const spi_config spi4_config = {
     .sc_reg_base = SPI4_BASE,
-    .sc_sck  = { GPIOE,  GPIO2, GPIO_AF5 },
-    .sc_miso = { GPIOE,  GPIO5, GPIO_AF5 },
-    .sc_mosi = { GPIOE,  GPIO6, GPIO_AF5 },
+    .sc_sck      = { GPIOE, GPIO2, GPIO_AF5 },
+    .sc_miso     = { GPIOE, GPIO5, GPIO_AF5 },
+    .sc_mosi     = { GPIOE, GPIO6, GPIO_AF5 },
+    .sc_rx_dma   = { DMA2,  DMA_STREAM0, 4 },
+    .sc_tx_dma   = { DMA2,  DMA_STREAM1, 4 },
 };
 
 static const spi_config spi5_config = {
     .sc_reg_base = SPI5_BASE,
-    .sc_sck  = { GPIOF,  GPIO7, GPIO_AF5 },
-    .sc_miso = { GPIOF,  GPIO8, GPIO_AF5 },
-    .sc_mosi = { GPIOF,  GPIO9, GPIO_AF5 },
+    .sc_sck      = { GPIOF, GPIO7, GPIO_AF5 },
+    .sc_miso     = { GPIOF, GPIO8, GPIO_AF5 },
+    .sc_mosi     = { GPIOF, GPIO9, GPIO_AF5 },
+    .sc_rx_dma   = { DMA2,  DMA_STREAM5, 7 },
+    .sc_tx_dma   = { DMA2,  DMA_STREAM6, 7 },
 };
 
 static const spi_config *config_map[7] = {
@@ -103,12 +138,6 @@ static size_t group_count = sizeof group_ss_pins / sizeof group_ss_pins[0];
 
 static void spi_setup_pin(const gpio_pin *pin, int pupd)
 {
-    // printf("gpio_mode_setup(port=%#lx, mode=%#x, updown=%#x, gpios=%#x)\n",
-    //        pin->gp_port,
-    //        GPIO_MODE_AF,
-    //        GPIO_PUPD_NONE,
-    //        pin->gp_pin);
-
     gpio_mode_setup(pin->gp_port,
                     GPIO_MODE_AF,
                     pupd,
@@ -139,6 +168,12 @@ static void spi_setup_config(const spi_config *config)
            SPI_CR1_CPHA_CLK_TRANSITION_2,
            SPI_CR1_DFF_8BIT,
            SPI_CR1_MSBFIRST);
+#if RX_DMA
+    spi_enable_rx_dma(config->sc_reg_base);
+#endif
+#if TX_DMA
+    spi_enable_tx_dma(config->sc_reg_base);
+#endif
     spi_init_master(config->sc_reg_base,
                     SPI_CR1_BAUDRATE_FPCLK_DIV_256,
                     SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE,
@@ -156,7 +191,6 @@ static void spi_setup_config(const spi_config *config)
 
 void spi_setup()
 {
-    printf("%s:%d: %s\n", __FILE__, __LINE__, __func__);
     // Clocks.
     // rcc_periph_clock_enable(RCC_SPI1);
     // rcc_periph_clock_enable(RCC_SPI3);
@@ -169,37 +203,30 @@ void spi_setup()
     rcc_periph_clock_enable(RCC_GPIOE);
     rcc_periph_clock_enable(RCC_GPIOF);
 
-    printf("%s:%d: %s\n", __FILE__, __LINE__, __func__);
+#if RX_DMA || TX_DMA
+    // rcc_periph_clock_enable(RCC_DMA1);
+    rcc_periph_clock_enable(RCC_DMA2);
+#endif
     
     // spi_setup_config(&spi1_config);
     // spi_setup_config(&spi3_config);
     // spi_setup_config(&spi4_config);
     spi_setup_config(&spi5_config);
-    printf("%s:%d: %s\n", __FILE__, __LINE__, __func__);
 
     // Configure nSS pins
     for (size_t i = 0; i < group_count; i++) {
         const gpio_pin *gp = &group_ss_pins[i];
-        printf("%s:%d: %s: gpio_mode_setup(%#lx, %#x, %#x, %#x)\n",
-               __FILE__, __LINE__, __func__,
-               gp->gp_port,
-               GPIO_MODE_OUTPUT,
-               GPIO_PUPD_NONE,
-               gp->gp_pin);
         gpio_mode_setup(gp->gp_port,
                         GPIO_MODE_OUTPUT,
                         GPIO_PUPD_NONE,
                         gp->gp_pin);
-        printf("%s:%d: %s\n", __FILE__, __LINE__, __func__);
         gpio_set(gp->gp_port, gp->gp_pin);
     }
-    printf("%s:%d: %s\n", __FILE__, __LINE__, __func__);
 
     // Hold pins PC1 and PC2 high.
     gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1 | GPIO2);
     gpio_set(GPIOC, GPIO1);
     gpio_set(GPIOC, GPIO2);
-    printf("%s:%d: %s\n", __FILE__, __LINE__, __func__);
 }
 
 void spi_select_group(int group)
@@ -227,27 +254,94 @@ void spi_start_transfer(int            spi,
     const spi_config *config = config_map[spi];
     assert(config);
     uint32_t base = config->sc_reg_base;
+#if RX_DMA
+    uint32_t rx_dma = config->sc_rx_dma.dc_dma;
+    uint8_t rx_stream = config->sc_rx_dma.dc_stream;
+    uint32_t rx_channel = config->sc_rx_dma.dc_channel;
+#endif
     
-    // printf("spi_start_transfer: config = %p\n", config);
-    // printf("                    base   = %#lx\n", base);
+#if RX_DMA
+    //  1. Clear SxCR_EN.  Wait until SxCR_EN == 0.
+    dma_stream_reset(rx_dma, rx_stream);
+    while (DMA_SCR(rx_dma, rx_stream) & DMA_SxCR_EN)
+        continue;
+
+    //  2. Set peripheral port address register, SxPAR.
+    dma_set_peripheral_address(rx_dma, rx_stream, (uint32_t)&SPI_DR(base));
+
+    //  3. Set memory address register, SxMA0R.
+    dma_set_memory_address(rx_dma, rx_stream, (uint32_t)rx_buf);
+
+    //  4. Set number of data items, SxNDTR
+    dma_set_number_of_data(rx_dma, rx_stream, count);
+
+    //  5. Set the dma channel using SxCR.CHSEL[2:0].
+    uint32_t rx_chsel = (uint32_t)rx_channel << DMA_SxCR_CHSEL_SHIFT;
+    dma_channel_select(rx_dma, rx_stream, rx_chsel);
+
+    //  6. Set SxCR.PFCTRL.
+    dma_set_peripheral_flow_control(rx_dma, rx_stream);
+
+    //  7. Set the stream priority.
+    dma_set_priority(rx_dma, rx_stream, DMA_SxCR_PL_LOW);
+
+    //  8. Set FIFO mode: disabled.
+    dma_enable_direct_mode(rx_dma, rx_stream);
+
+    //  9. Set data transfer direction,
+    //         peripheral and memory increment/fixed mode,
+    //         single or burst transactions,
+    //         peripheral and memory data widths,
+    //         circular mode,
+    //         double buffer mode,
+    //         and interrupts
+    //     in the SxCR register.
+    dma_disable_peripheral_increment_mode(rx_dma, rx_stream);
+    dma_enable_memory_increment_mode(rx_dma, rx_stream);
+    dma_set_peripheral_burst(rx_dma, rx_stream, DMA_SxCR_PBURST_SINGLE);
+    dma_set_memory_burst(rx_dma, rx_stream, DMA_SxCR_MBURST_SINGLE);
+    dma_set_peripheral_size(rx_dma, rx_stream, DMA_SxCR_PSIZE_8BIT);
+    dma_set_memory_size(rx_dma, rx_stream, DMA_SxCR_MSIZE_8BIT);
+    dma_set_transfer_mode(rx_dma, rx_stream, DMA_SxCR_DIR_PERIPHERAL_TO_MEM);
+    dma_disable_double_buffer_mode(rx_dma, rx_stream);
+    dma_disable_transfer_error_interrupt(rx_dma, rx_stream);
+    dma_disable_half_transfer_interrupt(rx_dma, rx_stream);
+    dma_disable_transfer_complete_interrupt(rx_dma, rx_stream);
+    dma_disable_direct_mode_error_interrupt(rx_dma, rx_stream);
+    dma_disable_fifo_error_interrupt(rx_dma, rx_stream);
+    printf("%s:%d: %s: DMA_S%dCR = %#lx\n",
+           __FILE__, __LINE__, __func__,
+           rx_stream, DMA_SCR(rx_dma, rx_stream));
+
+    // 10. Set SxCR_EN.
+    dma_enable_stream(rx_dma, rx_stream);
+    
+#endif
     spi_enable(config->sc_reg_base);
-    // printf("%s:%d: %s: SPI_CR1 = %#lx\n",
-    //        __FILE__, __LINE__, __func__,
-    //        SPI_CR1(base));
     for (size_t i = 0; i < count; i++) {
 	while (!(SPI_SR(base) & SPI_SR_TXE))
             continue;
 	SPI_DR(base) = tx_buf[i];
+#if !RX_DMA
         while (!(SPI_SR(base) & SPI_SR_RXNE))
             continue;
         rx_buf[i] = SPI_DR(base);
-        // for (int i = 0; i < 200; i++)
-        //     __asm__("nop");
-        // printf("                 %u: SR = %#lx\n", i, SPI_SR(base));
+#endif
     }
+    // See Section 28.3.8, Disabling the SPI.
+    while (!(SPI_SR(base) & SPI_SR_RXNE))
+        continue;
+    while (!(SPI_SR(base) & SPI_SR_TXE))
+        continue;
+    while (SPI_SR(base) & SPI_SR_BSY)
+        continue;
+#if RX_DMA
+    DMA_SCR(rx_dma, rx_stream) &= ~DMA_SxCR_EN;
+    while (DMA_SCR(rx_dma, rx_stream) & DMA_SxCR_EN)
+        continue;
+#endif
     spi_disable(base);
 }
-
 
 void spi_finish_transfer(int spi)
 {
